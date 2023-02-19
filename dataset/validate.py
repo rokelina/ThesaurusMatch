5404d422bc817c219804c5b1ed25c53809f50b71
Rosina Scampino <rosinascampino@gmail.com>
2023-02-19 17:38:48 -0500

    read csv file

--- dataset/validate.py
+++ dataset/validate.py
@@ -1,1 +1,22 @@
 import json
+import csv
+
+wordsInputPath = 'dataset/unigram_freq.csv'
+wordsArray = []
+
+thesaurusInputPath = 'dataset/en_thesaurus.jsonl'
+thesaurusOutputPath = '/Users/rosinascampino/Desktop/ReactNativeCourses/synonymGame/thesaurus.json'
+
+
+def getWords(inputFile):
+    with open(inputFile, 'r') as file:
+        reader = csv.reader(file)
+        output = []
+        for row in reader:
+            name = row[0]
+            count = int(row[1])
+            d = {'name': name, 'count': count}
+            output.append(d)
+
+    outputSorted = sorted(output, key=lambda x: x['count'], reverse=True)
+    return outputSorted
