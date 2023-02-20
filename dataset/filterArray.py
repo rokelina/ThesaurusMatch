9e6adf98983b685956900d7f6762cbc0987c10b6
Rosina Scampino <rosinascampino@gmail.com>
2023-02-20 11:01:35 -0500

    cleaned json file

---
+++ dataset/filterArray.py
@@ -0,0 +1,64 @@
+countryNames = [
+    'afghanistan', 'albania', 'algeria', 'andorra', 'angola', 'antigua and barbuda', 'argentina', 'armenia', 'australia', 'austria', 'azerbaijan',
+    'bahamas', 'bahrain', 'bangladesh', 'barbados', 'belarus', 'belgium', 'belize', 'benin', 'bhutan', 'bolivia', 'bosnia and herzegovina', 'botswana', 'brazil', 'brunei', 'bulgaria', 'burkina faso', 'burundi',
+    'cabo verde', 'cambodia', 'cameroon', 'canada', 'central african republic', 'chad', 'chile', 'china', 'colombia', 'comoros', 'congo', 'costa rica', 'croatia', 'cuba', 'cyprus', 'czechia',
+    'denmark', 'djibouti', 'dominica', 'dominican republic', 'ecuador', 'egypt', 'el salvador', 'equatorial guinea', 'eritrea', 'estonia', 'eswatini', 'ethiopia',
+    'fiji', 'finland', 'france', 'gabon', 'gambia', 'georgia', 'germany', 'ghana', 'greece', 'grenada', 'guatemala', 'guinea', 'guinea-bissau', 'guyana',
+    'haiti', 'honduras', 'hungary', 'iceland', 'india', 'indonesia', 'iran', 'iraq', 'ireland', 'israel', 'italy', 'ivory coast', 'jamaica', 'japan', 'jordan',
+    'kazakhstan', 'kenya', 'kiribati', 'kosovo', 'kuwait', 'kyrgyzstan', 'laos', 'latvia', 'lebanon', 'lesotho', 'liberia', 'libya', 'liechtenstein', 'lithuania', 'luxembourg', 'madagascar', 'malawi', 'malaysia', 'maldives',
+    'mali', 'malta', 'marshall islands', 'mauritania', 'mauritius', 'mexico', 'micronesia', 'moldova', 'monaco', 'mongolia', 'montenegro', 'morocco', 'mozambique', 'myanmar',
+    'namibia', 'nauru', 'nepal', 'netherlands', 'new zealand', 'nicaragua', 'niger', 'nigeria', 'north korea', 'north macedonia', 'norway',
+    'oman', 'pakistan', 'palau', 'panama', 'papua new guinea', 'paraguay', 'peru', 'philippines', 'poland', 'portugal', 'qatar', 'romania', 'russia', 'rwanda',
+    'saint kitts and nevis', 'saint lucia', 'saint vincent and the grenadines', 'samoa', 'san marino', 'sao tome and principe', 'saudi arabia', 'senegal', 'serbia', 'seychelles', 'sierra leone', 'singapore', 'slovakia', 'slovenia',
+    "slovenia", "solomon islands", "somalia", "south africa", "south korea", "south sudan", "spain", "sri lanka", "sudan", "suriname", "sweden", "switzerland", "syria", "taiwan", "tajikistan",    "tanzania", "thailand", "timor-leste (east timor)",
+    "togo", "tonga", "trinidad and tobago", "tunisia", "turkey", "turkmenistan", "tuvalu", "uganda",
+    "ukraine", "united arab emirates", "united kingdom", "united states of america", "uruguay", "uzbekistan", "vanuatu", "vatican city", "venezuela", "vietnam", "yemen", "yugoslavia" "zambia",
+    "zimbabwe"]
+
+
+usStatesCities = [
+    'alabama', 'birmingham', 'montgomery', 'mobile', 'alaska', 'anchorage', 'fairbanks', 'juneau', 'arizona', 'phoenix', 'tucson', 'mesa', 'arkansas', 'little rock', 'fort smith', 'fayetteville', 'california', 'los angeles',
+    'san francisco', 'san diego', 'colorado', 'denver', 'colorado springs', 'aurora', 'connecticut', 'bridgeport', 'new haven', 'hartford', 'delaware', 'wilmington', 'dover', 'newark', 'florida', 'miami', 'orlando', 'tampa',
+    'georgia', 'atlanta', 'savannah', 'augusta', 'hawaii', 'honolulu', 'hilo', 'kailua', 'idaho', 'boise', 'meridian', 'nampa', 'illinois', 'chicago', 'springfield', 'rockford', 'indiana', 'indianapolis', 'fort wayne', 'evansville',
+    'iowa', 'des moines', 'cedar rapids', 'davenport', 'kansas', 'wichita', 'overland park', 'kansas city', 'kentucky', 'louisville', 'lexington', 'bowling green', 'louisiana', 'new orleans', 'baton rouge', 'shreveport', 'maine',
+    'portland', 'lewiston', 'augusta', 'maryland', 'baltimore', 'frederick', 'annapolis', 'massachusetts', 'boston', 'worcester', 'springfield', 'michigan', 'detroit', 'grand rapids', 'ann arbor', 'minnesota', 'minneapolis', 'saint paul',
+    'rochester', 'mississippi', 'jackson', 'gulfport', 'hattiesburg', 'missouri', 'kansas city', 'st. louis', 'springfield', 'montana', 'billings', 'missoula', 'great falls', 'nebraska', 'omaha', 'lincoln', 'bellevue', 'nevada', 'las vegas', 'henderson',
+    'reno', 'new hampshire', 'manchester', 'nashua', 'concord', 'new jersey', 'newark', 'jersey city', 'trenton', 'new mexico', 'albuquerque', 'santa fe', 'las cruces', 'new york', 'new york city', 'buffalo', 'rochester', 'north carolina', 'charlotte',
+    'raleigh', 'greensboro', 'north dakota', 'fargo', 'bismarck', 'grand forks', 'ohio', 'columbus', 'cleveland', 'cincinnati', 'oklahoma', 'oklahoma city', 'tulsa', 'norman', 'oregon', 'portland', 'eugene', 'salem', 'pennsylvania', 'philadelphia', 'pittsburgh',
+    'allentown', 'rhode island', 'providence', 'warwick', 'cranston', 'south carolina', 'charleston', 'columbia', 'greenville', 'south dakota', 'sioux falls', 'rapid city', 'aberdeen', 'tennessee', 'nashville', 'memphis', 'knoxville', 'texas', 'houston',
+    'san antonio', 'dallas', 'utah', 'salt lake city', 'west valley city', 'provo', 'vermont', 'burlington', 'south burlington', 'rutland', 'virginia', 'virginia beach', 'norfolk', 'richmond', 'washington', 'seattle', 'spokane', 'tacoma', 'west virginia', 'charleston',
+    'huntington', 'parkersburg', 'wisconsin', 'milwaukee', 'madison', 'green bay', 'wyoming', 'cheyenne', 'casper', 'laramie']
+
+codes = [
+    "usa",
+    "nyc",
+    "lax",
+    "gop",
+    "dnc",
+    "cia",
+    "fbi",
+    "dot",
+    "com",
+    "edu",
+    "gov",
+    "mil",
+    "net",
+    "org",
+    "msn",
+    "npr",
+    "pbs",
+    "sec",
+    "aaa",
+    "abc",
+    "wow"
+]
+
+us_name_file = '/Users/rosinascampino/Desktop/names_project/raw_data/usa/yob2021.txt'
+names = []
+
+with open(us_name_file, 'r') as file:
+    for row in file:
+        line = row.split(',')
+        names.append(line[0])
+
+filterWords = countryNames + usStatesCities + codes + names
--- dataset/validate.py
+++ dataset/validate.py
@@ -1,22 +1,41 @@
 import json
 import csv
+from filterArray import filterWords
 
 wordsInputPath = 'dataset/unigram_freq.csv'
 
-
 thesaurusInputPath = 'dataset/en_thesaurus.jsonl'
-thesaurusOutputPath = '/Users/rosinascampino/Desktop/ReactNativeCourses/synonymGame/thesaurus.json'
+thesaurusOutputPath = 'dataset/thesaurus.json'
 
 
-def getWords(inputFile):
+def getWords(inputFile, filterList):
     with open(inputFile, 'r') as file:
         reader = csv.reader(file)
         next(reader, None)
-        output = [row[0] for row in reader]
-        words = output[0:10000]
+        output = [row[0] for row in reader if len(
+            row[0]) > 2 and row[0] not in filterList]
+        words = output[0:5000]
 
     return words
 
 
-wordsArray = getWords(wordsInputPath)
-print(wordsArray[0])
+def getThesaurus(inputFile, outputFile, wordList):
+    output = []
+    # open jsonl, parse each line to a python dictionary
+    with open(inputFile, 'r') as inFile, open(outputFile, 'w') as outFile:
+        for line in inFile:
+            obj = json.loads(line)
+    # if x['word'] in wordlList and x['synonyms'] != []
+            if obj['word'] in wordList and obj['synonyms'] != []:
+                newObj = {
+                    'word': obj['word'], 'pos': obj['pos'], 'synonyms': obj['synonyms']}
+                output.append(newObj)
+        # sort words and dump them to a json file
+        sortedOutput = sorted(output, key=lambda x: x['word'])
+        json.dump(sortedOutput, outFile)
+    return sortedOutput
+
+
+wordsArray = getWords(wordsInputPath, filterWords)
+
+thesaurus = getThesaurus(thesaurusInputPath, thesaurusOutputPath, wordsArray)
