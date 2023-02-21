aeec68c416227bbc16eb5a97e030f467946d95ee
Rosina Scampino <rosinascampino@gmail.com>
2023-02-21 12:46:56 -0500

    get random object, prompt

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 import data from './dataset/thesaurus.json' assert { type: 'json' };
-console.log(data);
 
-//on program start load data into memory, return an object with a
-//random word as a property and an array of all synonyms as the value
-
-function load() {
+function load(arr) {
   //generate a random index
-  let randomIndex;
-  //get the word object located at that index in data
-  let randomWord;
+  const randomIndex = Math.floor(Math.random() * arr.length + 1);
+  //get the object located at that index in data and store the value of 'word'
+  const randomWord = data[randomIndex].word;
   //get all the synonyms available for that word
-  let synonymArray;
-
-  return (outputWord = {
-    randomWord: synonymArray,
+  let synonymArray = [];
+  //look at the whole array and find all the synonyms for randomWord
+  arr.forEach((obj) => {
+    if (obj.word === randomWord) {
+      synonymArray.push(obj.synonyms);
+    }
   });
+  const outputWord = {
+    word: randomWord,
+    synonyms: synonymArray.flat(),
+  };
+  return outputWord;
 }
 
 function play(outputWord) {
   //takes the object returned by load()
-  //generates a prompt object that displays the word propery
+  //generates a prompt object that displays the object.word value
+  let inputSynonym = prompt(`What's a synonym for "${outputWord.word}"?`);
 
-  let inputSynonym = prompt(`What's a synonym for ${outputWord.key}?`);
-
-  //user input. on click "ok" will compare input with outputWord and alert('message')
-  if (outputWord.values.includes(inputSynonym.toLowerCase())) {
+  //checks if input is a valid synonym
+  if (outputWord.synonyms.includes(inputSynonym.trim().toLowerCase())) {
     alert('Correct!');
   } else {
     alert("That word doesn't seem to be a synonym");
   }
 }
+
+const word = load(data);
+play(word);
