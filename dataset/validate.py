868584ceeb3e58b4ca621d34e2d12aa73bcc9785
Rosina Scampino <rosinascampino@gmail.com>
2023-02-23 09:05:51 -0500

    rename, add more words to json file

--- dataset/validate.py
+++ dataset/validate.py
@@ -2,7 +2,7 @@ import json
 import csv
 from filterArray import filterWords
 
-wordsInputPath = 'dataset/unigram_freq.csv'
+frequentWordsPath = 'dataset/unigram_freq.csv'
 
 thesaurusInputPath = 'dataset/en_thesaurus.jsonl'
 thesaurusOutputPath = 'dataset/thesaurus.json'
@@ -14,19 +14,19 @@ def getWords(inputFile, filterList):
         next(reader, None)
         output = [row[0] for row in reader if len(
             row[0]) > 2 and row[0] not in filterList]
-        words = output[0:5000]
+        words = output[0:10000]
 
     return words
 
 
-def getThesaurus(inputFile, outputFile, wordList):
+def getThesaurus(inputFile, outputFile, frequentWordsFile):
     output = []
     # open jsonl, parse each line to a python dictionary
     with open(inputFile, 'r') as inFile, open(outputFile, 'w') as outFile:
         for line in inFile:
             obj = json.loads(line)
-    # if x['word'] in wordlList and x['synonyms'] != []
-            if obj['word'] in wordList and obj['synonyms'] != []:
+            # if x['word'] in wordlList and x['synonyms'] != []
+            if obj['word'] in frequentWordsFile and obj['synonyms'] != []:
                 newObj = {
                     'word': obj['word'], 'pos': obj['pos'], 'synonyms': obj['synonyms']}
                 output.append(newObj)
@@ -36,6 +36,5 @@ def getThesaurus(inputFile, outputFile, wordList):
     return sortedOutput
 
 
-wordsArray = getWords(wordsInputPath, filterWords)
-
+wordsArray = getWords(frequentWordsPath, filterWords)
 thesaurus = getThesaurus(thesaurusInputPath, thesaurusOutputPath, wordsArray)
